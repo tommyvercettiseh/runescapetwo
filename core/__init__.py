30110dc8760696a93c4b5ff4e300da04e76bb1b1
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-07-25 19:31:53 +0200

    Expose vision module

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,10 @@
-from . import keyboard, mouse
+from . import keyboard, mouse, vision
 from .profile import active_profile_name, load_profile
 
-__all__ = ["mouse", "keyboard", "load_profile", "active_profile_name"]
+__all__ = [
+    "mouse",
+    "keyboard",
+    "vision",
+    "load_profile",
+    "active_profile_name",
+]
