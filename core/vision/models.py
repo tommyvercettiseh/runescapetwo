165be2a722c53c9cae216184b64c9d9f1aabb37c
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-07-26 09:34:04 +0200

    Add colour blob model

--- core/vision/models.py
+++ core/vision/models.py
@@ -40,3 +40,30 @@ class Hit(MatchResult):
             return self.center
 
         return random.randint(left, right), random.randint(top, bottom)
+
+
+@dataclass(frozen=True)
+class ColourBlob:
+    """One connected colour region in absolute screen coordinates."""
+
+    x: int
+    y: int
+    width: int
+    height: int
+    area_px: float
+    centroid_x: int
+    centroid_y: int
+
+    @property
+    def center(self) -> tuple[int, int]:
+        return self.centroid_x, self.centroid_y
+
+    def random_point(self, padding: int = 0) -> tuple[int, int]:
+        padding = max(0, int(padding))
+        left = self.x + padding
+        top = self.y + padding
+        right = self.x + self.width - padding - 1
+        bottom = self.y + self.height - padding - 1
+        if right < left or bottom < top:
+            return self.center
+        return random.randint(left, right), random.randint(top, bottom)
