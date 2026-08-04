54a7ff19871dbd3b9281c1b40525419aecfa660a
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-04 20:46:33 +0200

    Fix image tester launch paths

--- Start Image Tester.bat
+++ Start Image Tester.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 if not exist ".venv\Scripts\python.exe" py -m venv .venv || goto :error
 call ".venv\Scripts\activate.bat" || goto :error
 python -m pip install --quiet --disable-pip-version-check -r requirements.txt || goto :error
-python -m tools.image_tester.gui
+python -m tools.image_tester.app
 exit /b 0
 :error
 echo Image Tester kon niet worden gestart.
