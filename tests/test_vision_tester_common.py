b6d16f5176c7f96abc7052c23ef8b11d02289b7f
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-05 08:24:18 +0200

    Fix rounded control startup crash

--- tests/test_vision_tester_common.py
+++ tests/test_vision_tester_common.py
@@ -1,4 +1,4 @@
-from tools.vision_tester.common import filter_options
+from tools.vision_tester.common import _rounded_rectangle, filter_options
 
 
 def test_filter_options_matches_any_part_case_insensitively() -> None:
@@ -13,3 +13,17 @@ def test_filter_options_returns_everything_for_empty_query() -> None:
     values = ["one", "two"]
 
     assert filter_options(values, "  ") == values
+
+
+def test_rounded_rectangle_accepts_canvas_outline_width() -> None:
+    class FakeCanvas:
+        def create_polygon(self, *coordinates, **options):
+            self.coordinates = coordinates
+            self.options = options
+            return 1
+
+    canvas = FakeCanvas()
+
+    _rounded_rectangle(canvas, 116, 36, 12, outline="#000", width=1)
+
+    assert canvas.options["width"] == 1
