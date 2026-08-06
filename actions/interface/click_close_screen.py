776f191a892810a5750b6082cd1c2807c5502b22
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-06 11:00:58 +0200

    Add general screen close action

---
+++ actions/interface/click_close_screen.py
@@ -0,0 +1,19 @@
+from core import mouse_actions
+
+
+SCREEN_CROSS_IMAGE = "ScreenCross"
+SCREEN_AREA = "Bot_Area"
+
+SCREEN_BUTTON = "left"
+SCREEN_EDGE_PADDING = 2
+
+
+def click_close_screen(bot_id: int = 1):
+    return mouse_actions.click_image(
+        image_name=SCREEN_CROSS_IMAGE,
+        area_name=SCREEN_AREA,
+        bot_id=bot_id,
+        button=SCREEN_BUTTON,
+        image_edge_padding=SCREEN_EDGE_PADDING,
+        confirm_before_click=True,
+    )
