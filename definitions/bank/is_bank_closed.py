ca6a9bfe8a61064ec08cf2a3567f316378ccd470
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-06 12:46:32 +0200

    Make bank closed the inverse of bank open

--- definitions/bank/is_bank_closed.py
+++ definitions/bank/is_bank_closed.py
@@ -1,9 +1,5 @@
 from definitions.bank.is_bank_open import is_bank_open
-from definitions.interface.is_screen_open import is_screen_open
 
 
 def is_bank_closed(bot_id: int = 1) -> bool:
-    return (
-        not is_bank_open(bot_id)
-        and not is_screen_open(bot_id)
-    )
+    return not is_bank_open(bot_id)
