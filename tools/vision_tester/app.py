15ab814f500509eb1ae194d2852eebbc199f6bef
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-07 09:48:39 +0200

    Install simplified colour preview layout

--- tools/vision_tester/app.py
+++ tools/vision_tester/app.py
@@ -1,8 +1,10 @@
 from .template_plus import install_template_plus
 from .area_overlay_toggle import install_area_overlay_toggle
+from .colour_view_cleanup import install_colour_view_cleanup
 
 install_template_plus()
 install_area_overlay_toggle()
+install_colour_view_cleanup()
 
 from .unified_plus import VisionTester, main
 
