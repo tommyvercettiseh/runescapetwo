cad7b0cd8f2b816af9014fdb5278a2713fb23f34
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-07 11:31:04 +0200

    Enable colour recording and replay mode

--- tools/vision_tester/app.py
+++ tools/vision_tester/app.py
@@ -1,17 +1,19 @@
 from .template_plus import install_template_plus
 from .area_overlay_toggle import install_area_overlay_toggle
 from .colour_view_cleanup import install_colour_view_cleanup
-from .hotkey_fix import install_hotkey_fix
 
 install_template_plus()
 install_area_overlay_toggle()
 install_colour_view_cleanup()
-install_hotkey_fix()
 
 from . import unified_plus
 from .colour_browser import install_colour_browser
 
 install_colour_browser()
+
+from .colour_recording import install_colour_recording
+
+install_colour_recording()
 
 VisionTester = unified_plus.VisionTester
 main = unified_plus.main
