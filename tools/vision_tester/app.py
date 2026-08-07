44b172fe81ac9f8a0417e297e4571b57578ebfce
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-07 09:02:26 +0200

    Enable optional area overlay toggle

--- tools/vision_tester/app.py
+++ tools/vision_tester/app.py
@@ -1,6 +1,8 @@
 from .template_plus import install_template_plus
+from .area_overlay_toggle import install_area_overlay_toggle
 
 install_template_plus()
+install_area_overlay_toggle()
 
 from .unified_plus import VisionTester, main
 
