bd5951622c62b4612391f88d974e93e3daa316ec
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-30 13:34:50 +0200

    Use bank object preset area for open sensor

--- definitions/bank/is_bank_open.py
+++ definitions/bank/is_bank_open.py
@@ -1,10 +1,12 @@
 from core import vision
-from definitions.bank.bank_target import BANK_AREA, BANK_DEPOSIT_IMAGE
+from core.vision.object_presets import load_object_preset
+from definitions.bank.bank_target import BANK_DEPOSIT_IMAGE
 
 
 def is_bank_open(bot_id: int = 1) -> bool:
+    preset = load_object_preset("bank")
     return vision.find_image(
         image_name=BANK_DEPOSIT_IMAGE,
-        area=BANK_AREA,
+        area=preset.area,
         bot_id=bot_id,
     ) is not None
