fdc863ff9f971c7821d3be77d43f2362065fb85e
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-30 13:22:43 +0200

    Route click_furnace through object presets

--- actions/furnace/click_furnace.py
+++ actions/furnace/click_furnace.py
@@ -1,21 +1,5 @@
-from core import mouse_actions
-
-
-FURNACE_COLOUR = "purple"
-FURNACE_AREA = "Bot_Area"
-FURNACE_MIN_PIXELS = 250
-FURNACE_MAX_PIXELS = 4000
-FURNACE_EDGE_PADDING = 20
-FURNACE_BUTTON = "left"
+from actions.click_object import click_object
 
 
 def click_furnace(bot_id: int = 1):
-    return mouse_actions.click_colour(
-        colour_name=FURNACE_COLOUR,
-        area_name=FURNACE_AREA,
-        bot_id=bot_id,
-        button=FURNACE_BUTTON,
-        minimum_blob_pixels=FURNACE_MIN_PIXELS,
-        maximum_blob_pixels=FURNACE_MAX_PIXELS,
-        blob_edge_padding=FURNACE_EDGE_PADDING,
-    )
+    return click_object("furnace", bot_id=bot_id)
