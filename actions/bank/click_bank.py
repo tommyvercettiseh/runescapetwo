76f0a58e902139ba189a7dd935551f95fa689aa8
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-30 13:22:38 +0200

    Route click_bank through object presets

--- actions/bank/click_bank.py
+++ actions/bank/click_bank.py
@@ -1,21 +1,5 @@
-from core import mouse_actions
-from definitions.bank.bank_target import (
-    BANK_AREA,
-    BANK_BUTTON,
-    BANK_COLOUR,
-    BANK_EDGE_PADDING,
-    BANK_MAX_PIXELS,
-    BANK_MIN_PIXELS,
-)
+from actions.click_object import click_object
 
 
 def click_bank(bot_id: int = 1):
-    return mouse_actions.click_colour(
-        colour_name=BANK_COLOUR,
-        area_name=BANK_AREA,
-        bot_id=bot_id,
-        button=BANK_BUTTON,
-        minimum_blob_pixels=BANK_MIN_PIXELS,
-        maximum_blob_pixels=BANK_MAX_PIXELS,
-        blob_edge_padding=BANK_EDGE_PADDING,
-    )
+    return click_object("bank", bot_id=bot_id)
